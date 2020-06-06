b3d57e3d5592f2282da083238ecb6023938ef12d
Marci <matusekma@gmail.com>
2020-06-06 10:42:12 +0200

    Setup Spring backend

---
+++ guess-the-image-backend/src/test/java/com/mata/guesstheimagebackend/GuessTheImageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mata.guesstheimagebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessTheImageBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
