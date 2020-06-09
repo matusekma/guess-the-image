20927682e2bbf9b8cb2439ab00c15b06468a1349
Marci <matusekma@gmail.com>
2020-06-09 14:41:29 +0200

    Check token expiration on private routes

--- guess-the-image-frontend/src/components/PrivateRoute.tsx
+++ guess-the-image-frontend/src/components/PrivateRoute.tsx
@@ -1,33 +1,50 @@
 import React, { useEffect } from "react";
 import { Redirect, Route, RouteProps, useLocation } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import jwt_decode from "jwt-decode";
+
 import Navbar from "./Navbar";
 import { PropsFromRedux } from "../containers/PrivateRouteContainer";
-import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../reducers/rootReducer";
 import { loginSuccess } from "../actions/auth/actionCreators";
 import { history } from "../App";
+import { axiosInstance } from "../apiCalls/axiosConfig";
 
 interface PrivateRouteProps extends RouteProps {}
+
+function checkTokenNotExpired(token: string) {
+  const decodedToken: {
+    exp: number;
+    iat: number;
+  } = jwt_decode(token);
+  return decodedToken.exp > new Date().getTime() / 1000;
+}
 
 function PrivateRoute({
   children,
   isAuthenticated,
   ...rest
 }: PrivateRouteProps & PropsFromRedux & any) {
-  const token = useSelector((state: RootState) => state.auth.token);
+  const reduxToken = useSelector((state: RootState) => state.auth.token);
   const location = useLocation();
   const dispatch = useDispatch();
 
   // HANDLE REFRESH
   useEffect(() => {
-    if (!token) {
-      const storageToken = localStorage.getItem("token");
-      if (storageToken) {
-        dispatch(loginSuccess(storageToken));
+    try {
+      let token = reduxToken;
+      if (!token) {
+        token = localStorage.getItem("token");
+      }
+      if (token && checkTokenNotExpired(token)) {
+        dispatch(loginSuccess(token));
+        axiosInstance.defaults.headers["Authorization"] = `Bearer ${token}`;
         history.push(location.pathname);
       }
+    } catch {
+      // Invalid token, do nothing
     }
-  }, []);
+  }, [dispatch, reduxToken]);
 
   return (
     <Route
